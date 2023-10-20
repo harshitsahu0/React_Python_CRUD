effc69be04b0e4d16e76deaa07f8ca79fbdb2ac9
harshitsahu0 <harshitsahu409@gmail.com>
2023-10-20 12:49:19 +0530

    first commit

---
+++ react_crud/src/Components/DataTable/DataTable.jsx
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Space } from 'antd';
+import swal from 'sweetalert';
+import "./DataTable.css"
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+
+const DataTable = (props) => {
+    const [search, setSearch] = useState("");
+    const [current, setCurrent] = useState(1);
+    const [searchRst, setSearchRst] = useState([]);
+    const [srchState, setSrchState] = useState(false);
+    const itemPerpage = 10;
+
+    const totalItemPerPages = props.data.length;
+    const totalPages = Math.ceil(totalItemPerPages / itemPerpage)
+
+    const getPageNumbers = () => {
+        const totalNumPages = [];
+        for (let i = 1; i <= totalPages; i++) {
+            totalNumPages.push(i);
+        }
+        return totalNumPages;
+    }
+
+    useEffect(() => {
+        getPageNumbers();
+    }, []);
+
+    const handleDelete = (id) => {
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this Data",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+
+                swal("successfull!", "Your details deleted!", "success");
+                axios.delete(`http://127.0.0.1:8000/api/companies/${id}/`).then((res) => {
+                    console.log(res);
+                    props.getData();
+                })
+            } else {
+                swal("Your Data is safe!");
+            }
+        });
+    }
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+        var values = props.data.filter((item) => item.name === e.target.value);
+        setSearchRst(values);
+        console.log(values);
+        if (values.length === 0) {
+            setSrchState(false);
+        } else {
+            setSrchState(true);
+        }
+    }
+
+    return (
+        <div style={{ marginTop: "10px" }} className='col-lg-11 col-md-12 col-sm-12 mainDataTable'>
+            <div className='mainSearchDiv col-lg-6'>
+                <input type="text" placeholder='Search company...' id='srchInpt' value={search} onChange={handleSearch} />
+            </div>
+            {
+                srchState ?
+                    < table className="table table-success table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Company Name</th>
+                                <th scope="col">description</th>
+                                <th scope="col">founded_date</th>
+                                <th scope="col">headquarters_location</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {searchRst.map((item, i) => {
+                                const itemIndex = (current - 1) * itemPerpage + i + 1;
+                                return (
+                                    <tr key={item.id}>
+                                        <th scope="row">{itemIndex}</th>
+                                        <td>{item.name}</td>
+                                        <td>{item.description}</td>
+                                        <td>{item.founded_date}</td>
+                                        <td>{item.headquarters_location}</td>
+                                        <td>
+                                            <Space wrap>
+                                                <button data-bs-toggle="modal" data-bs-target="#exampleModal" id='updateBtn' onClick={() => props.handleUpdateState(item.name, item.description, item.founded_date, item.headquarters_location, item.id)}>
+                                                    <EditOutlined />
+                                                </button>
+                                                <button type="primary" id='deleteBtn' onClick={() => handleDelete(item.id)}><DeleteOutlined /> </button>
+                                            </Space>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                    :
+                    props.data.length > 0 ?
+                        <>
+                            <table className="table table-success table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Company Name</th>
+                                        <th scope="col">description</th>
+                                        <th scope="col">founded_date</th>
+                                        <th scope="col">headquarters_location</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {props.data.slice((current - 1) * itemPerpage, current * itemPerpage).map((item, i) => {
+                                        const itemIndex = (current - 1) * itemPerpage + i + 1;
+                                        return (
+                                            <tr key={item.id}>
+                                                <th scope="row">{itemIndex}</th>
+                                                <td>{item.name}</td>
+                                                <td>{item.description}</td>
+                                                <td>{item.founded_date}</td>
+                                                <td>{item.headquarters_location}</td>
+                                                <td>
+                                                    <Space wrap>
+                                                        <button data-bs-toggle="modal" data-bs-target="#exampleModal" id='updateBtn' onClick={() => props.handleUpdateState(item.name, item.description, item.founded_date, item.headquarters_location, item.id)}>
+                                                            <EditOutlined />
+                                                        </button>
+                                                        <button type="primary" id='deleteBtn' onClick={() => handleDelete(item.id)}><DeleteOutlined /> </button>
+                                                    </Space>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                            <div className="pagination col-lg-3">
+                                {totalPages > 1 && (
+                                    <div className='mainPage'>
+                                        <button onClick={() => setCurrent(current - 1)} className='next-prev-btn' disabled={current === 1}>
+                                            Previous
+                                        </button>
+                                        {getPageNumbers().map((pageNumber) => (
+                                            <button key={pageNumber} className={pageNumber === current ? 'active' : ''} onClick={() => setCurrent(pageNumber)} >
+                                                {pageNumber}
+                                            </button>
+                                        ))}
+                                        <button onClick={() => setCurrent(current + 1)} className='next-prev-btn' disabled={current === totalPages}>
+                                            Next
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        </>
+                        : ""
+            }
+        </div >
+    )
+}
+
+export default DataTable
---
+++ react_crud/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react'
+import DataTable from '../DataTable/DataTable';
+import "./NavBar.css";
+import axios from 'axios';
+import swal from 'sweetalert';
+
+
+const NavBar = () => {
+
+    const [comName, setComName] = useState("");
+    const [ComDescription, setComDescription] = useState("");
+    const [foundDate, setFoundDate] = useState("");
+    const [headLocation, setHeadLocation] = useState("");
+    const [update, setUpdate] = useState(false);
+    const [id, setId] =useState();
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        axios.get('http://127.0.0.1:8000/api/companies/').then((res) => {
+            setData(res.data);
+        })
+    }
+
+    const handleUpdate = () => {
+        const formData = new FormData();
+        formData.append("name", comName);
+        formData.append("description", ComDescription);
+        formData.append("founded_date", foundDate);
+        formData.append("headquarters_location", headLocation);
+        axios.put(`http://127.0.0.1:8000/api/companies/${id}/`,formData).then((res) => {
+            console.log(res);
+            swal("successfull!", "Your details updated!", "success");
+            getData();
+        })
+        console.log("id :",id);
+        console.log("hit");
+        const submitBtn = document.getElementById("createBtn");
+        submitBtn.innerHTML = "Submit";
+    }
+
+    const handleUpdateState = (name, des, date, loc, id) => {
+        setComName(name);
+        setComDescription(des);
+        setFoundDate(date);
+        setHeadLocation(loc);
+        const submitBtn = document.getElementById("createBtn");
+        submitBtn.innerHTML = "Update";
+        setUpdate(true);
+        setId(id);
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const handleFromSubmit = () => {
+        if (update) {
+            console.log("id in submit function : ", id);
+            handleUpdate()
+            setComName("");
+            setComDescription("");
+            setFoundDate("");
+            setHeadLocation("");
+        } else {
+            const formData = new FormData();
+            formData.append("name", comName);
+            formData.append("description", ComDescription);
+            formData.append("founded_date", foundDate);
+            formData.append("headquarters_location", headLocation);
+            axios.post("http://127.0.0.1:8000/api/companies/", formData).then((res) => {
+                swal("successfull!", "Your details Submited!", "success");
+                getData();
+            })
+            setComName("");
+            setComDescription("");
+            setFoundDate("");
+            setHeadLocation("");
+        }
+    }
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#e3f2fd" }}>
+                <div className="container-fluid">
+                    <h3>CRUD Operation</h3>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        </ul>
+                        <form className="d-flex">
+                            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Add Company
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+            <DataTable data={data} getData={getData} handleUpdateState={handleUpdateState} />
+
+            <div className="modal fade " id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <h5> Company Name:</h5>
+                                        </td>
+                                        <td><input type="text" id="first_name" placeholder="Enter company_Name" value={comName} onChange={(e) => setComName(e.target.value)} />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h5>description:</h5>
+                                        </td>
+                                        <td><input type="text" id="last_name" placeholder="company description" value={ComDescription} onChange={(e) => setComDescription(e.target.value)} />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h5>founded_date :</h5>
+                                        </td>
+                                        <td>
+                                            <input type="date" id="Dob" placeholder="Enter Your Date of birth" value={foundDate} onChange={(e) => setFoundDate(e.target.value)} />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h5>headquarters_location:</h5>
+                                        </td>
+                                        <td>
+                                            <select name="Country" id="Country" value={headLocation} onChange={(e) => setHeadLocation(e.target.value)}>
+                                                <option value="" selected disabled>select Location</option>
+                                                <option value="Indore">Indore</option>
+                                                <option value="Bhopal">Bhopal</option>
+                                                <option value="Pune">Pune</option>
+                                                <option value="Bangalore">Bangalore</option>
+                                                <option value="Mumbai">Mumbai</option>
+                                                <option value="Hyderabad">Hyderabad</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" id='createBtn' className="btn btn-primary" data-bs-dismiss="modal" onClick={handleFromSubmit}>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
